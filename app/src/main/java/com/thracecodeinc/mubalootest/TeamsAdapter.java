8f7f67f064cb8ec8b429502ba54ae4b99cea3178
Sami <YOUR EMAIL ADDRESSsmail.ali@mail.mail.citytech,cuny.edu>
2016-06-29 18:43:37 +0300

    teams grouping (RecycleView)

--- app/src/main/java/com/thracecodeinc/mubalootest/TeamsAdapter.java
+++ app/src/main/java/com/thracecodeinc/mubalootest/TeamsAdapter.java
@@ -1,9 +1,8 @@
 package com.thracecodeinc.mubalootest;
 
 import android.content.Context;
-import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +20,6 @@ import java.util.ArrayList;
  * Created by Samurai on 6/28/16.
  */
 public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder>{
-
     private Context mContext;
     private OnItemClickListener mItemClickListener;
     private ArrayList<Team> teamsList;
@@ -38,6 +36,7 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder>{
         public FrameLayout placeNameHolder;
         public TextView placeName;
         public ImageView placeImage;
+        public TextView leaderText;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -45,6 +44,7 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder>{
             placeName = (TextView) itemView.findViewById(R.id.firstName);
             placeNameHolder = (FrameLayout) itemView.findViewById(R.id.placeNameHolder);
             placeImage = (ImageView) itemView.findViewById(R.id.placeImage);
+            leaderText = (TextView) itemView.findViewById(R.id.leader_id);
             placeHolder.setOnClickListener(this);
 
         }
@@ -77,6 +77,8 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder>{
     // 2
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_teams, parent, false);
         return new ViewHolder(view);
     }
@@ -85,6 +87,16 @@ public class TeamsAdapter extends RecyclerView.Adapter<TeamsAdapter.ViewHolder>{
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         final Team team = teamsList.get(position);
+        if (team.isTeamLead()) {
+            holder.placeNameHolder.setBackgroundColor(ContextCompat.getColor(mContext, R.color.colorAccent));
+            holder.leaderText.setText(mContext.getString(R.string.team_leader));
+        }
+        else {
+            holder.placeNameHolder.setBackgroundColor(ContextCompat.getColor(mContext, R.color.primary_dark));
+            holder.leaderText.setText(team.getRole());
+        }
+
+
         String fullName = team.getFirstName()+" "+team.getLastName();
         holder.placeName.setText(fullName);
         Picasso.with(mContext)
